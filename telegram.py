efe1afd493b636de08d576f2ef96bd910c6635fb
PercevalSA <perceval.arenou@bilberry.io>
2025-07-06 17:42:16 +0200

    testing more telegram

--- telegram.py
+++ telegram.py
@@ -2,12 +2,12 @@
 
 import argparse
 
-from telethon.sync import TelegramClient, events
+from telethon import TelegramClient
 
-# /!\ attention au ToS, on va mettre un max de slip
-# la session est stockée dans un fichier .session portant le nom du client
-# si le fichier existe déjà, on n'a pas besoin de se réauthentifier
-
+# telethon session is stored in a file named after the client
+# if the file already exists, we don't need to re-authenticate
+# the session file is named <client_name>.session
+# as we will send messages on 2 users' behalf, we will need to create 2 clients
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Telegram Bot Example")
@@ -17,18 +17,62 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def main():
-    args = parse_arguments()
-    with TelegramClient(args.name, args.api_id, args.api_hash) as client:
-        client.send_message("me", "Hello, myself!")
-        print(client.download_profile_photo("me"))
+async def tg():
+    # Getting information about yourself
+    me = await client.get_me()
 
-        @client.on(events.NewMessage(pattern="(?i).*Hello"))
-        async def handler(event):
-            await event.reply("Hey!")
+    # "me" is a user object. You can pretty-print
+    # any Telegram object with the "stringify" method:
+    print(me.stringify())
 
-        client.run_until_disconnected()
+    # When you print something, you see a representation of it.
+    # You can access all attributes of Telegram objects with
+    # the dot operator. For example, to get the username:
+    username = me.username
+    print(username)
+    print(me.phone)
+
+    # You can print all the dialogs/conversations that you are part of:
+    # async for dialog in client.iter_dialogs():
+    #     print(dialog.name, "has ID", dialog.id)
+
+    # You can send messages to yourself...
+    await client.send_message("me", "Hello, myself!")
+    # ...to your contacts
+    await client.send_message("+33600000", "Hello, friend!")
+    # ...or even to any username
+    await client.send_message("username", "Testing Telethon!")
+
+    # You can, of course, use markdown in your messages:
+    message = await client.send_message(
+        "me",
+        "This message has **bold**, `code`, __italics__ and "
+        "a [nice website](https://example.com)!",
+        link_preview=False,
+    )
+
+    # Sending a message returns the sent message object, which you can use
+    print(message.raw_text)
+
+    # You can reply to messages directly if you have a message object
+    await message.reply("Cool!")
+
+    # Or send files, songs, documents, albums...
+    await client.send_file("me", "image.jpg")
+
+    # You can print the message history of any chat:
+    async for message in client.iter_messages("me"):
+        print(message.id, message.text)
+
+        # You can download media from messages, too!
+        # The method will return the path where the file was saved.
+        if message.photo:
+            path = await message.download_media()
+            print("File saved to", path)  # printed after download is done
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+
+    with TelegramClient(args.name, args.api_id, args.api_hash) as client:
+        client.loop.run_until_complete(tg())
